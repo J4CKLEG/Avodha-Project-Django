ae19391dd6b582c1e0ca8b01b58db79a245e15f3
akhil <akhiljoy375@gmail.com>
2023-04-05 10:46:24 +0530

    half compl001

--- shop/views.py
+++ shop/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import *
 from django.db.models import Q
 from django.core.paginator import Paginator, EmptyPage, InvalidPage
-from cart.views import cart
+# from cart.views import cart
 
 
 # Create your views here.
